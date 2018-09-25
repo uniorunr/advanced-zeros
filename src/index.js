e01186e2f797878414ed05e43610dd6cecb38fc6
Yury C <gagael23@live.com>
2018-09-26 00:33:19 +0300

    advanced-zeros: completed task

--- src/index.js
+++ src/index.js
@@ -1,34 +1,57 @@
 module.exports = function getZerosCount(number, base) {
   if (1 <= number && number <= 100000000) {
 
-    var factors = [];
-    var currentRemainder = base;
+    let factors = [];
+    let currentRemainder = base;
 
-    for (var i = 2; i <= currentRemainder; i++) { // finding the factors of the base
+    for (let i = 2; i <= currentRemainder; i++) { // finding the factors of the base
       while (currentRemainder % i === 0) {
           factors.push(i);
           currentRemainder = currentRemainder / i;
       }
     }
     
-    var exponent = 1; // default exponent
+    const factorsLength = factors.length;
+    let uniqueFactors = []; // array of unique factors
+    let uniqueExponents = []; // array of exponents of factors
 
-    for (var n = (factors.length - 1); n > 0; n--) { // finding the exponent of the max factor
-      if (factors[n] === factors[n - 1]) {
-        exponent = exponent + 1;
-      } else break;
+    for (let i = 0; i < factorsLength; i++) { // finding the unique factors
+      if (uniqueFactors.indexOf(factors[i]) === -1) {
+        uniqueFactors.push(factors[i]);
+      }
     }
 
-    var maxFactor = factors[factors.length - 1];
-    var counter = Math.floor(number / maxFactor);
-    var currentCount = counter;
-    
-    while (currentCount >= maxFactor) {
-      currentCount = currentCount / maxFactor; // counting how many times number contains the max factor
-      counter = counter + Math.floor(currentCount);
-    } 
+    let counterOfExp = 1;
 
-    return Math.floor(counter / exponent); // the count of the max factors divided by the exponent
+    for (let i = 0; i < factorsLength; i++) { // finding the exponents of factors
+      if (factors[i] === factors[i + 1]) {
+        counterOfExp++;
+      } else if (factors[i]) {
+        uniqueExponents.push(counterOfExp);
+        counterOfExp = 1;
+      } else {
+        uniqueExponents.push(counterOfExp);
+        uniqueExponents.push(1);
+      }
+    }
+
+    let countersForCompare = [];
+
+    for (let i = 0; i < uniqueFactors.length; i++) { // counting how many times number contains the factor
+      let factor = uniqueFactors[i];
+      let counter = Math.floor(number / factor);
+      let currentCount = counter;
+      let exponent = uniqueExponents[i] || 1;
+      
+      while (currentCount >= factor) {
+        currentCount = currentCount / factor; 
+        counter = counter + Math.floor(currentCount);
+      } 
+      countersForCompare.push(Math.floor(counter / exponent)); // pushing result to the array for compare
+    }
+
+    countersForCompare.sort((a, b) => b - a);
+    return countersForCompare[countersForCompare.length - 1]; // returning of min result
   
   } else {
     return {error: 'Enter a number from 1 to 100000000'};
